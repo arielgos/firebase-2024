c4ae85a0f4eec52fbeed34823925c0feb33a7c9a
Ariel Ortuño Solis <arielg.os@gmail.com>
2024-06-25 22:12:16 -0400

    Adding test and coverage

--- public/src/App.test.js
+++ public/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders page', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  const linkElement = screen.getByText(/Signed/i)
   expect(linkElement).toBeInTheDocument()
 })
+
