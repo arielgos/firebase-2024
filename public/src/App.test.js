a20a14a59756680e6d7e59f333d984d04a68d386
Ariel Ortuño Solis <arielg.os@gmail.com>
2024-06-25 22:16:09 -0400

    Code Coverage

--- public/src/App.test.js
+++ public/src/App.test.js
@@ -3,7 +3,7 @@ import App from './App'
 
 test('renders page', () => {
   render(<App />)
-  const linkElement = screen.getByText(/Signed/i)
+  const linkElement = screen.getByText(/Sign in/i)
   expect(linkElement).toBeInTheDocument()
 })
 
